6b5ec4d67c1ff98b284e5dd0866193a475ea7152
adamoudad <adam.oudad@gmail.com>
2022-11-18 07:57:04 +0900

    Minor fixes on random_aphorisms and youtube-share-tracker

--- random_aphorisms.py
+++ random_aphorisms.py
@@ -6,9 +6,12 @@ import os
 
 os.environ['DBUS_SESSION_BUS_ADDRESS'] = 'unix:path=/run/user/1000/bus'
 
-def get_aphorisms(path):
+def get_file_content(path, split_lines=False):
     with open(path, mode='r') as f:
-        return f.readlines()
+        if split_lines:
+            return f.read().split("\n")
+        else:
+            return f.read()
 
 
 notify2.init('Random aphorism')
@@ -16,11 +19,24 @@ notify2.init('Random aphorism')
 def notify(message):
     notify2.Notification(message).show()
     
-def random_aphorism(path):
-    aphorisms = get_aphorisms(path)
-    return aphorisms[random.randint(1,len(aphorisms)-1)].strip()
+def random_aphorism(path=Path().home() / "Datasets/aphorisms.txt"):
+    aphorisms = get_file_content(path, split_lines=True)
+    aphorism = aphorisms[random.randint(0,len(aphorisms)-1)].strip()
+    text, author = aphorism.split(" -- ")
+    return text, author
+
+def get_note(path=Path().home() / "Tests/note.txt"):
+    note = get_file_content(path, split_lines=False)
+    with open(path, "w") as f:
+        f.write("")
+    return note
+
 
 if __name__=="__main__":
-    path = Path().home() / "Datasets/aphorisms.txt"
-    aphorism = random_aphorism(path)
-    notify(aphorism)
+    aphorism, author = random_aphorism()
+    note = get_note()
+    notify(aphorism + "\n\n" + author)
+    if note:
+        notify(note)
+    print("Aphorism", aphorism)
+    print("Note", note)
